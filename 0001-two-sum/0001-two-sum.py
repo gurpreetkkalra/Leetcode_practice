03da2220fa981b7ada54abb7713e4d92b8e23b0c
gurpreetkkalra <154840249+gurpreetkkalra@users.noreply.github.com>
2025-05-14 17:46:38 -0700

    Time: 2032 ms (38.01%), Space: 13.2 MB (61.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,7 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        hmap={}
-        for i in range(len(nums)):
-            dif=target-nums[i]
-            if dif in hmap:
-                return [hmap[dif],i]
-            hmap[nums[i]]=i
-        return
-                
+        for i in range (len(nums)):
+            for j in range (i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+               
