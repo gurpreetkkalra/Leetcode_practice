2490c1e5e878c14cb72c9f19afe03d9bba30f0c7
gurpreetkkalra <154840249+gurpreetkkalra@users.noreply.github.com>
2025-05-20 14:09:50 -0700

    Time: 3 ms (92%), Space: 13.1 MB (34%) - LeetHub

---
+++ 2398-check-if-matrix-is-x-matrix/2398-check-if-matrix-is-x-matrix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def checkXMatrix(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+
+        for i in range (m):
+            for j in range (n):
+                if (i == j or i+j == n-1):
+                    if (grid[i][j] == 0):
+                        return False
+                else:
+                    if(grid[i][j] != 0):
+                        return False
+
+        return True
+                    
