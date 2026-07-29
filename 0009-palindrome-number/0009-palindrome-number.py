56f38f304857d0064a73bac56db9c1809ed3d7bc
gurpreetkkalra <154840249+gurpreetkkalra@users.noreply.github.com>
2026-07-29 13:27:07 -0700

    Time: 11 ms (55.57%), Space: 12.3 MB (89.95%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,7 +1,12 @@
 class Solution(object):
     def isPalindrome(self, x):
-        if(str(x)==str(x)[::-1]):
-            return True
-        else:
+        if x < 0:
             return False
+        n = x
+        sum = 0
+        while n != 0:
+            rem = n%10
+            sum = sum*10 + rem
+            n = n//10
+        return sum == x
         
